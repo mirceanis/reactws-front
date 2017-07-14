d56ed377c5a9db4354ddb0f92e066e50f1dfed0a
Mircea NISTOR <mirceanis@gmail.com>
2017-07-14 20:20:04 +0300

    add an example test

---
+++ android/app/src/test/java/com/reactws/MainActivityTest.java
@@ -0,0 +1,24 @@
+package com.reactws;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by mnistor on 7/14/17.
+ */
+public class MainActivityTest extends TestCase {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testGetMainComponentName() throws Exception {
+        assertTrue(true);
+    }
+
+}
